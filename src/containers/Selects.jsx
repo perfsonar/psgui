0b03df01ac6a4738faf9831fa76b7c9b55fcc9a1
ljubomir <ljubomir@carnet.hr>
2022-10-24 14:19:14 +0200

    Upgraded yarn. Added Dockerfile. Bug resolved overwritten nodes.

--- src/containers/Selects.jsx
+++ src/containers/Selects.jsx
@@ -112,7 +112,7 @@ class Selects extends Component {
       .then(
         (items) => {
           let options = items.rows.map(item => ({ label: item.name,  value: item.id}));
-          Object.assign(options, TestDefaultValues.geant_nodes.rows);
+          Array.prototype.push.apply(options, TestDefaultValues.geant_nodes.rows);
           options.sort(function(a, b) {
             var labelA = a.label.toUpperCase();
             var labelB = b.label.toUpperCase();
